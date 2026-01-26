eb964fcb7f34e46a460c38f3da2bdfd410aa14c6
krishnakasaudhan0 <kasaudhankrishna05@gmail.com>
2026-01-26 11:41:47 +0000

    style: format utility helpers

--- src/utils.js
+++ src/utils.js
@@ -6,3 +6,5 @@
 // Git contribution logic update at: 2026-01-23T14:08:33.000Z [hash: b0ou58]
 
 // Git contribution logic update at: 2026-01-26T03:42:14.000Z [hash: qwv0bb]
+
+// Git contribution logic update at: 2026-01-26T11:41:47.000Z [hash: 00m0jf]
